4ff4193298e8fba247fa6d1f8aa552ee48187ba7
Renan Santana <renan.santana.ferreira.1@gmail.com>
2023-10-25 22:27:31 -0300

    feat: adding exerc_04 lab03

--- Programacao Estruturada/lab03/exerc_04.c
+++ Programacao Estruturada/lab03/exerc_04.c
@@ -1,84 +1,52 @@
 #include <stdio.h>
-#define MAX 1000
 
-void tira_final(char texto[MAX])
-{
-    for (int i = 0; i < MAX; i++)
-    {
-        if (texto[i] == '\n')
-        {
-            texto[i] = '\0';
-            break;
-        }
+#define MAX 100
+
+int verificaPalavraNaFrase(char palavra[], char frase[]) {
+    int i, j, k;
+    int tamPalavra = 0;
+    int tamFrase = 0;
+
+    for (i = 0; palavra[i] != '\0'; i++) {
+        tamPalavra++;
     }
-}
 
-int len(char texto[MAX])
-{
-    int cont = 0, i = 0;
-    while (i < MAX && texto[i] != '\0')
-    {
-        cont = cont + 1;
-        i = i + 1;
+    for (i = 0; frase[i] != '\0'; i++) {
+        tamFrase++;
     }
-    return cont - 1;
-}
 
-int compara_palavras(int inicio, char p1[MAX], char p2[MAX])
-{
-    int i = 0;
-    if (len(p1) != len(p2))
-        return 0;
-
-    while (p1[i] != '\0' && p2[i] != '\0' && i < MAX)
-    {
-        if (p1[i] != p2[i])
-            return 0;
-        i = i + 1;
-    }
-    return 1;
-}
-
-int p_frase(char frase[MAX], char p[MAX])
-{
-    int i = 0;
-    while (frase[i] != '\0')
-    {
-        int k = 0;
-        while (frase[i] != ' ' && frase[i] != '\0')
-        {
-
-            if (frase[i] != p[k])
+    for (i = 0; i <= tamFrase - tamPalavra; i++) {
+        int r = 1;
+        for (j = 0; j < tamPalavra; j++) {
+            if (frase[i + j] != palavra[j]) {
+                r = 0;
                 break;
-            k = k + 1;
-            i = i + 1;
+            }
         }
-
-        if (p[k] == '\0')
-        {
+        if (r) {
             return 1;
-        }
-
-        if (frase[i] == ' ')
-        {
-            i = i + 1;
         }
     }
 
     return 0;
 }
 
-char frase[MAX], palavra[MAX];
-int r;
-int main()
-{
-    printf("Running\n");
-    fgets(frase, MAX, stdin);
-    tira_final(frase);
-    fgets(palavra, MAX, stdin);
-    tira_final(palavra);
+int main() {
+    char palavra[MAX];
+    char frase[MAX];
 
-    r = p_frase(frase, palavra);
-    printf("%d\n", r);
+    printf("Digite uma palavra: ");
+    scanf("%s", palavra);
+
+    printf("Digite uma frase: ");
+    getchar(); // Consumir o caractere de nova linha pendente
+    fgets(frase, MAX, stdin);
+
+    if (verificaPalavraNaFrase(palavra, frase)) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
     return 0;
 }
