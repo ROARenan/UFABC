c0b877c44dc0683f1267815e689ff5c3b5b449ea
Renan Santana <renan.santana.ferreira.1@gmail.com>
2023-12-06 07:56:53 -0300

    chore: atualizações dos aquivos de teste

--- Programacao Estruturada/lab07/meugrep.c
+++ Programacao Estruturada/lab07/meugrep.c
@@ -2,10 +2,14 @@
 
 int main(int argc, char *argv[]) {
     FILE *text;
-    char aux;
+    char *aux;
+    int cont = 0, linha = 1, r = 0;
     if (argc < 2) {
         printf("Forneca os arquivos separados por espaco");
         return 1;
+    }
+    while (cont != '\n'){
+        cont++;
     }
 
     for (int i = 1; i < argc; i++) {
@@ -14,8 +18,17 @@ int main(int argc, char *argv[]) {
             printf("Erro ao abrir o arquivo %d",i);
         }
 
-         while (fscanf(text, "%c", &aux) != EOF) {
-            printf("%c",aux);
+         while (fscanf(text, "%s", aux) != EOF) {
+            printf("%s ",aux);
+            if (aux == argv[0][r]){
+                r++;
+            }
+            else{
+                r = 0;
+            }
+            if (aux == '\n'){
+                linha++;
+            }
         }
         printf("\n");
         fclose(text);
