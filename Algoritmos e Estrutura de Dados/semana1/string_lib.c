b221c481e942059d5fd483d3bf2dbd5dc26aca51
ROARenan <renan.santana.ferreira.1@gmail.com>
2024-02-20 20:44:10 -0300

    chore: finalizando codigos semana 1

--- Algoritmos e Estrutura de Dados/semana1/string_lib.c
+++ Algoritmos e Estrutura de Dados/semana1/string_lib.c
@@ -3,12 +3,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-String* string_create(char* data, int length) {
-    String* str = malloc(sizeof(String));
+String *string_create(char *data, int length)
+{
+    String *str = malloc(sizeof(String));
 
     str->data = malloc(length * sizeof(char));
 
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         str->data[i] = data[i];
     }
 
@@ -17,29 +19,44 @@ String* string_create(char* data, int length) {
     return str;
 }
 
-String* string_reverse(String str){
-    String* back_str = malloc(sizeof(String));
-    back_str->data = malloc(str.length * sizeof(char));
+String *string_reverse(String *str)
+{
+    String *back_str = malloc(sizeof(String));
+    back_str->data = malloc(str->length * sizeof(char));
 
-    int j = str.length;
+    int j = str->length - 1;
 
-    for (int i = 0; i < str.length; i++){
-        back_str->data[i] = str.data[j];
+    for (int i = 0; i < str->length; i++)
+    {
+        back_str->data[i] = str->data[j];
         j--;
     }
+    back_str->length = str->length;
+
+    return back_str;
 }
 
-void string_append(String *str1, String str2){
-    
+void string_append(String *str1, String *str2)
+{
+    str1->data = realloc(str1->data, (str1->length + str2->length) * sizeof(char));
+
+    for (int i = 0; i < str2->length; i++)
+    {
+        str1->data[str1->length + i] = str2->data[i];
+    }
+    str1->length += str2->length;
 }
 
-void string_destroy(String* str) {
+void string_destroy(String *str)
+{
     free(str->data);
     free(str);
 }
 
-void string_print(String* str) {
-    for (int i = 0; i < str->length; i++) {
+void string_print(String *str)
+{
+    for (int i = 0; i < str->length; i++)
+    {
         printf("%c", str->data[i]);
     }
     printf("\n");
--- Algoritmos e Estrutura de Dados/semana1/string_lib_main.c
+++ Algoritmos e Estrutura de Dados/semana1/string_lib_main.c
@@ -2,6 +2,30 @@
 #include "string_lib.h"
 
 int main(void) {
-    /* code */
+    // Criando duas strings
+    String* str1 = string_create("Hello", 5);
+    String* str2 = string_create(" World!", 7);
+
+    // Imprimindo as strings originais
+    printf("String 1: ");
+    string_print(str1);
+    printf("String 2: ");
+    string_print(str2);
+
+    // Revertendo a primeira string e imprimindo
+    String* reversed_str = string_reverse(str1);
+    printf("String 1 invertida: ");
+    string_print(reversed_str);
+
+    // Concatenando as duas strings e imprimindo
+    string_append(str1, str2);
+    printf("String concatenada: ");
+    string_print(str1);
+
+    // Destruindo as strings
+    string_destroy(str1);
+    string_destroy(str2);
+    string_destroy(reversed_str);
+
     return 0;
 }
